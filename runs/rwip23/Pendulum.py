ee946e86f7a9b0c409a38964cbbae0ac5bcb4fda
drib861204 <56364972+drib861204@users.noreply.github.com>
2021-11-30 22:50:49 +0800

    1130 trial 23 adequate mass and motor char

---
+++ runs/rwip23/Pendulum.py
@@ -0,0 +1,166 @@
+"""
+Title: pendulum_pygame
+Author: [jadenhensley](https://github.com/jadenhensley)
+Last modified: 2021/10/18
+Description: Pendulum project, built using pygame and math modules.
+
+Title: wheelPole
+Author: [aimetz](https://github.com/aimetz)
+Last modified: 2021/04/20
+
+Title: gym/gym/envs/classic_control/pendulum.py
+Author: [openai](https://github.com/openai)
+Last modified: 2021/10/31
+"""
+import pygame
+from math import pi, sin, cos
+import numpy as np
+
+
+class Pendulum:
+    def __init__(self, rend):
+        # paras according to paper [LQR and MPC controller design and comparison for a stationary self-balancing bicycle robot with a reaction wheel]
+        self.theta_rod = 0
+        self.theta_wheel = 0
+        self.theta_rod_dot = 0
+        self.theta_wheel_dot = 0
+        self.len_rod = 0.5
+        self.len_wheel = 0.5 #0.5, 0.75
+        self.rad_wheel = 0.1
+        self.mass_rod = 15
+        self.mass_wheel = 5
+        self.momentum_rod = self.mass_rod*self.len_rod**2/12
+        self.momentum_wheel = self.mass_wheel*self.rad_wheel**2 #depends on wheel shape
+        self.dt = 0.001
+        self.gravity = 9.8
+        self.wheel_max_speed = 20
+        self.torque = 0
+        self.voltage = 0
+
+        width = 800
+        height = 600
+        self.origin_x = width//2
+        self.origin_y = height//2
+        self.POS = np.array([self.origin_x, self.origin_y])
+
+        if rend:
+            pygame.init()
+            self.screen = pygame.display.set_mode((width, height))
+            pygame.display.set_caption("Pendulum Simulation")
+            pygame.font.init()
+            self.debug_font = pygame.font.SysFont('Bauhuas 93', 30)
+            self.hint_font = pygame.font.SysFont('Bauhaus 93', 26)
+            #print("font")
+
+
+    def reset(self):
+        roll_range = 10 #in degree
+        reset_max_speed = 3
+
+        self.theta_rod = (np.random.random()*2-1)*roll_range*pi/180
+        #self.theta_rod = pi/60
+        #self.theta_wheel = 0
+        self.theta_rod_dot = 0
+        #self.theta_rod_dot = (np.random.random() * 2 - 1) * reset_max_speed
+        self.theta_wheel_dot = 0
+        state = np.array([self.theta_rod, self.theta_rod_dot, self.theta_wheel_dot], dtype=np.float32)
+        return state
+
+
+    def render(self, eval_run):
+        #torque = action
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+        SCALE = 100
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+        RED = (255, 0, 0)
+        GRAY = (128, 128, 128)
+
+        tip_x = self.POS[0]+self.len_wheel*sin(self.theta_rod)*SCALE
+        tip_y = self.POS[1]-self.len_wheel*cos(self.theta_rod)*SCALE
+        POSTIP = np.array([tip_x, tip_y])
+        POSWHEEL = np.array(([tip_x+self.rad_wheel*sin(self.theta_wheel)*SCALE, tip_y+self.rad_wheel*cos(self.theta_wheel)*SCALE]))
+        #print(POSTIP)
+        self.screen.fill(WHITE)
+        pygame.draw.line(self.screen, BLACK, self.POS, POSTIP, 10)
+        pygame.draw.circle(self.screen, GRAY, POSTIP, self.rad_wheel*2*SCALE)
+        pygame.draw.circle(self.screen, RED, POSWHEEL, self.rad_wheel*2*SCALE//5)
+        img = self.hint_font.render("torque  : % .4f" %self.torque, True, BLACK)
+        img2 = self.hint_font.render("voltage: % .4f" %self.voltage, True, BLACK)
+        img3 = self.hint_font.render("Evaluation Run %d" %eval_run, True, BLACK)
+        self.screen.blit(img, (self.origin_x, self.origin_y/2-50))
+        self.screen.blit(img2, (self.origin_x, self.origin_y/2-30))
+        self.screen.blit(img3, (self.origin_x/5, self.origin_y/2-50))
+
+        pygame.display.update()
+
+
+    def step(self, action):
+        q1 = self.theta_rod
+        q2 = self.theta_wheel
+        q1_dot = self.theta_rod_dot
+        q2_dot = self.theta_wheel_dot
+        l1 = self.len_rod
+        l2 = self.len_wheel
+        m1 = self.mass_rod
+        m2 = self.mass_wheel
+        I1 = self.momentum_rod
+        I2 = self.momentum_wheel
+        dt = self.dt
+        g = self.gravity
+        gear_ratio = 7 #25
+        kt = 3.85 #0.0229
+        ke = 0.34 #0.0229
+        R = 0.38 #0.71
+        action_scale = 48
+
+        #torque = action
+        voltage = action * action_scale
+        torque = gear_ratio*kt/R*(voltage-ke*gear_ratio*q2_dot)
+        #print("v",voltage)
+        #print("T",torque)
+        #print("q2dot",q2_dot)
+
+        Ip = m1*l1**2+m2*l2**2+I1+I2
+        a = (m1*l1+m2*l2)*g*sin(angle_normalize(q1))
+
+        newq1_dot = q1_dot + ((a-torque)/(Ip-I2))*dt
+        #print("rod ang_vel",newq1_dot)
+        #newq1_dot = np.clip(newq1_dot, -self.wheel_max_speed, self.wheel_max_speed)
+        #print("rod ang_vel",newq1_dot)
+        newq1 = angle_normalize(angle_normalize(q1) + newq1_dot * dt)
+        #print("rod angle",newq1)
+
+        newq2_dot = q2_dot + ((torque*Ip-a*I2)/I2/(Ip-I2))*dt
+        newq2_dot = np.clip(newq2_dot, -self.wheel_max_speed, self.wheel_max_speed)
+        #print("wheel ang_vel",newq2_dot)
+        newq2 = angle_normalize(angle_normalize(q2) + newq2_dot * dt)
+        #print("wheel angle",newq2)
+
+        #print("torque",torque)
+        #print("\n")
+        #print([torque, newq1[0], newq2[0], newq1_dot[0], newq2_dot[0]])
+        state = np.array([newq1[0], newq1_dot[0], newq2_dot[0]], dtype=np.float32)
+        self.theta_rod = newq1
+        self.theta_wheel = newq2
+        self.theta_rod_dot = newq1_dot
+        self.theta_wheel_dot = newq2_dot
+        self.torque = torque
+        self.voltage = voltage
+
+        costs = 100 * angle_normalize(q1)**2 + 0.1 * q1_dot**2 + 0.001 * voltage**2
+
+        return state, -costs, False, {}
+
+
+    def close(self):
+        pygame.display.quit()
+        pygame.quit()
+
+
+def angle_normalize(th):
+    return ((th+pi)%(2*pi))-pi
---
+++ runs/rwip23/run.py
@@ -0,0 +1,212 @@
+import numpy as np
+import random
+#import gym
+#import pybulletgym # to run e.g. HalfCheetahPyBullet-v0
+#import pybullet_envs # to run e.g. HalfCheetahBullet-v0 different reward function bullet-v0 starts ~ -1500. pybullet-v0 starts at 0
+from collections import deque
+import torch
+import time
+from torch.utils.tensorboard import SummaryWriter
+import argparse
+from  files import MultiPro
+from files.Agent import Agent
+import json
+from Pendulum import * # added by Ben
+
+def timer(start,end):
+    """ Helper to print training time """
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("\nTraining Time:  {:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
+
+def evaluate(frame, eval_runs=5, capture=False, rend=False, savedmodel=False):
+    """
+    Makes an evaluation run with the current episode
+    """
+
+    reward_batch = []
+    for i in range(eval_runs):
+
+        state = eval_env.reset()
+        rewards = 0
+        rep = 0
+        rep_max = 200
+        if savedmodel:
+            rep_max = 5000
+        #action_v = 0
+
+        while True:
+
+            #print("eval")
+            #print(rend)
+
+            if rend:
+                #print("render")
+                # eval_env.render(mode="human")
+                eval_env.render(i+1)
+
+            action = agent.act(np.expand_dims(state, axis=0), eval=True)
+            action_v = np.clip(action, action_low, action_high)
+            state, reward, done, _ = eval_env.step(action_v[0])
+            rewards += reward
+            if done or rep >= rep_max:
+                break
+            rep += 1
+        reward_batch.append(rewards)
+    if capture == False and savedmodel == False:
+        writer.add_scalar("Reward", np.mean(reward_batch), frame)
+
+
+
+def run(args):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    i_episode = 1
+    state = envs.reset()
+    score = 0
+    frames = args.frames//args.worker
+    eval_every = args.eval_every//args.worker
+    eval_runs = args.eval_runs
+    worker = args.worker
+    ERE = args.ere
+    if ERE:
+        episode_K = 0
+        eta_0 = 0.996
+        eta_T = 1.0
+        #episodes = 0
+        max_ep_len = 500 # original = 1000
+        c_k_min = 2500 # original = 5000
+
+    rep = 0
+    for frame in range(1, frames+1):
+        # evaluation runs
+        #print("run")
+        rep += 1
+
+        if frame % eval_every == 0 or frame == 1:
+            evaluate(frame*worker, eval_runs, rend=args.render_evals)
+
+        action = agent.act(state)
+        action_v = np.clip(action, action_low, action_high)
+        next_state, reward, done, _ = envs.step(action_v) #returns np.stack(obs), np.stack(action) ...
+
+        #print(state, action, reward, next_state, done)
+        #for s, a, r, ns, d in zip(state, action, reward, next_state, done):
+        #    agent.step(s, a, r, ns, d, frame, ERE)
+        agent.step(state, action, reward, next_state, [done], frame, ERE)
+        
+        if ERE:
+            eta_t = eta_0 + (eta_T - eta_0)*(frame/(frames+1))
+            episode_K +=1
+        state = next_state
+        score += np.mean(reward)
+        
+        #if done.any():
+        if done or rep%200==0:
+            if ERE:
+                for k in range(1,episode_K):
+                    c_k = max(int(agent.memory.__len__()*eta_t**(k*(max_ep_len/episode_K))), c_k_min)
+                    agent.ere_step(c_k)
+            scores_window.append(score)       # save most recent score
+            scores.append(score)              # save most recent score
+            writer.add_scalar("Average100", np.mean(scores_window), frame*worker)
+            print('\rEpisode {}\tFrame: [{}/{}]\t Reward: {:.2f} \tAverage100 Score: {:.2f}'.format(i_episode*worker, frame*worker, frames, score, np.mean(scores_window)), end="", flush=True)
+            #if i_episode % 100 == 0:
+            #    print('\rEpisode {}\tFrame \tReward: {}\tAverage100 Score: {:.2f}'.format(i_episode*worker, frame*worker, round(eval_reward,2), np.mean(scores_window)), end="", flush=True)
+            i_episode +=1 
+            state = envs.reset()
+            score = 0
+            episode_K = 0              
+
+
+
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("-env", type=str,default="HalfCheetahBulletEnv-v0", help="Environment name, default = HalfCheetahBulletEnv-v0")
+parser.add_argument("-per", type=int, default=0, choices=[0,1], help="Adding Priorizied Experience Replay to the agent if set to 1, default = 0")
+parser.add_argument("-munchausen", type=int, default=0, choices=[0,1], help="Adding Munchausen RL to the agent if set to 1, default = 0")
+parser.add_argument("-dist", "--distributional", type=int, default=0, choices=[0,1], help="Using a distributional IQN Critic if set to 1, default=0")
+parser.add_argument("-ere", type=int, default=0, choices=[0,1], help="Adding Emphasizing Recent Experience to the agent if set to 1, default = 0")
+parser.add_argument("-n_step", type=int, default=1, help="Using n-step bootstrapping, default=1")
+parser.add_argument("-info", type=str, default="rwip", help="Information or name of the run")
+parser.add_argument("-d2rl", type=int, choices=[0,1], default=0, help="Uses Deep Actor and Deep Critic Networks if set to 1 as described in the D2RL Paper: https://arxiv.org/pdf/2010.09163.pdf, default=0")
+parser.add_argument("-frames", type=int, default=100000, help="The amount of training interactions with the environment, default is 1mio")
+parser.add_argument("-eval_every", type=int, default=1000, help="Number of interactions after which the evaluation runs are performed, default = 1000")
+parser.add_argument("-eval_runs", type=int, default=3, help="Number of evaluation runs performed, default = 1")
+parser.add_argument("-seed", type=int, default=0, help="Seed for the env and torch network weights, default is 0")
+parser.add_argument("--n_updates", type=int, default=1, help="Update-to-Data (UTD) ratio, updates taken per step with the environment, default=1")
+parser.add_argument("-lr_a", type=float, default=3e-4, help="Actor learning rate of adapting the network weights, default is 3e-4")
+parser.add_argument("-lr_c", type=float, default=3e-4, help="Critic learning rate of adapting the network weights, default is 3e-4")
+parser.add_argument("-a", "--alpha", type=float, help="entropy alpha value, if not choosen the value is leaned by the agent")
+parser.add_argument("-layer_size", type=int, default=256, help="Number of nodes per neural network layer, default is 256")
+parser.add_argument("-repm", "--replay_memory", type=int, default=int(1e6), help="Size of the Replay memory, default is 1e6")
+parser.add_argument("-bs", "--batch_size", type=int, default=256, help="Batch size, default is 256")
+parser.add_argument("-t", "--tau", type=float, default=0.005, help="Softupdate factor tau, default is 0.005")
+parser.add_argument("-g", "--gamma", type=float, default=0.99, help="discount factor gamma, default is 0.99")
+parser.add_argument("--saved_model", type=str, default=None, help="Load a saved model to perform a test run!")
+parser.add_argument("-w", "--worker", type=int, default=1, help="Number of parallel worker, default = 1")
+parser.add_argument("--render_evals", type=int, default=0, choices=[0,1], help="Rendering the evaluation runs if set to 1, default=0")
+parser.add_argument("--trial", type=int, default=0, help="trial")
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    if args.saved_model == None:
+        writer = SummaryWriter("runs/"+args.info+str(args.trial))
+    #envs = MultiPro.SubprocVecEnv([lambda: gym.make(args.env) for i in range(args.worker)])
+    #eval_env = gym.make(args.env)
+
+    envs = Pendulum(args.render_evals)
+    eval_env = Pendulum(args.render_evals)
+    '''
+    envs.seed(args.seed)
+    eval_env.seed(args.seed+1)
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    '''
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("Using device: {}".format(device))
+    '''
+    action_high = eval_env.action_space.high[0]
+    action_low = eval_env.action_space.low[0]
+    state_size = eval_env.observation_space.shape[0]
+    action_size = eval_env.action_space.shape[0]
+    '''
+    action_high = 1
+    action_low = -1
+    state_size = 3
+    action_size = 1
+
+    agent = Agent(state_size=state_size, action_size=action_size, args=args, device=device) 
+    
+    t0 = time.time()
+    if args.saved_model != None:
+        agent.actor_local.load_state_dict(torch.load(args.saved_model))
+        evaluate(frame=None, capture=False, rend=args.render_evals, savedmodel=True)
+    else:    
+        run(args)
+        t1 = time.time()
+        timer(t0, t1)
+
+        # save policy
+        torch.save(agent.actor_local.state_dict(), 'runs/{}{}/'.format(args.info,args.trial)+args.info+str(args.trial)+".pth")
+
+        # save parameter
+        with open('runs/{}{}/'.format(args.info,args.trial)+args.info+str(args.trial)+".json", 'w') as f:
+            json.dump(args.__dict__, f, indent=2)
+
+    eval_env.close()
+    if args.saved_model == None:
+        writer.close()
