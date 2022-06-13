126deefe31a1852ffdebe3c12c4593384218393b
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-13 18:03:24 +0800

    0613 change try.sh

--- try.sh
+++ try.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 run.py --trial 8 -seed 0 -frames 200000
-python3 run.py --trial 9 -seed 1 -frames 200000
-python3 run.py --trial 10 -seed 2 -frames 200000
-python3 run.py --trial 11 -seed 3 -frames 200000
-python3 run.py --trial 12 -seed 4 -frames 200000
+python3 run.py --trial 58 -seed 0 -frames 100000
+python3 run.py --trial 59 -seed 1 -frames 100000
+python3 run.py --trial 60 -seed 2 -frames 100000
+python3 run.py --trial 61 -seed 3 -frames 100000
+python3 run.py --trial 62 -seed 4 -frames 100000
