6d9d2756fbcb3c38b2b19186773ffa6a0ffc96b9
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-14 08:55:18 +0800

    0614 trial 60 lr 2e-3 61 1e-3

--- try.sh
+++ try.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-echo echo 1
-python3 run.py --trial 59 -seed 0 -frames 100000 -lr_a 3e-3 -lr_c 3e-3
-echo echo 2
-python3 run.py --trial 59 -seed 1 -frames 100000 -lr_a 3e-3 -lr_c 3e-3
-echo echo 3
-python3 run.py --trial 59 -seed 2 -frames 100000 -lr_a 3e-3 -lr_c 3e-3
-echo echo 4
-python3 run.py --trial 59 -seed 3 -frames 100000 -lr_a 3e-3 -lr_c 3e-3
-echo echo 5
-python3 run.py --trial 59 -seed 4 -frames 100000 -lr_a 3e-3 -lr_c 3e-3
+echo trial 60 echo 1
+python3 run.py --trial 60 -seed 0 -frames 100000 -lr_a 2e-3 -lr_c 2e-3
+echo trial 60 echo 2
+python3 run.py --trial 60 -seed 1 -frames 100000 -lr_a 2e-3 -lr_c 2e-3
+echo trial 60 echo 3
+python3 run.py --trial 60 -seed 2 -frames 100000 -lr_a 2e-3 -lr_c 2e-3
+echo trial 60 echo 4
+python3 run.py --trial 60 -seed 3 -frames 100000 -lr_a 2e-3 -lr_c 2e-3
+echo trial 60 echo 5
+python3 run.py --trial 60 -seed 4 -frames 100000 -lr_a 2e-3 -lr_c 2e-3
+
+echo trial 61 echo 1
+python3 run.py --trial 61 -seed 0 -frames 100000 -lr_a 1e-3 -lr_c 1e-3
+echo trial 61 echo 2
+python3 run.py --trial 61 -seed 1 -frames 100000 -lr_a 1e-3 -lr_c 1e-3
+echo trial 61 echo 3
+python3 run.py --trial 61 -seed 2 -frames 100000 -lr_a 1e-3 -lr_c 1e-3
+echo trial 61 echo 4
+python3 run.py --trial 61 -seed 3 -frames 100000 -lr_a 1e-3 -lr_c 1e-3
+echo trial 61 echo 5
+python3 run.py --trial 61 -seed 4 -frames 100000 -lr_a 1e-3 -lr_c 1e-3
