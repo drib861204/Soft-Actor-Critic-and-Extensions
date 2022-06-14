fff65d41112efa0e28d1034a8e9d22751b63fbdf
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-15 02:16:39 +0800

    0615 63 lr_a 3e-4 lr_c 2.5e-3

--- try.sh
+++ try.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo trial 62 echo 1
-python3 run.py --trial 62 -seed 0 -frames 100000 -lr_a 2.5e-3 -lr_c 2.5e-3
-echo trial 62 echo 2
-python3 run.py --trial 62 -seed 1 -frames 100000 -lr_a 2.5e-3 -lr_c 2.5e-3
-echo trial 62 echo 3
-python3 run.py --trial 62 -seed 2 -frames 100000 -lr_a 2.5e-3 -lr_c 2.5e-3
-echo trial 62 echo 4
-python3 run.py --trial 62 -seed 3 -frames 100000 -lr_a 2.5e-3 -lr_c 2.5e-3
-echo trial 62 echo 5
-python3 run.py --trial 62 -seed 4 -frames 100000 -lr_a 2.5e-3 -lr_c 2.5e-3
+echo trial 63 echo 1
+python3 run.py --trial 63 -seed 0 -frames 100000 -lr_c 2.5e-3
+echo trial 63 echo 2
+python3 run.py --trial 63 -seed 1 -frames 100000 -lr_c 2.5e-3
+echo trial 63 echo 3
+python3 run.py --trial 63 -seed 2 -frames 100000 -lr_c 2.5e-3
+echo trial 63 echo 4
+python3 run.py --trial 63 -seed 3 -frames 100000 -lr_c 2.5e-3
+echo trial 63 echo 5
+python3 run.py --trial 63 -seed 4 -frames 100000 -lr_c 2.5e-3
