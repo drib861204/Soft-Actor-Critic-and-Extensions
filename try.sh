ee4004a69fcabdd8a474f10d3b2c2d6753ec92b4
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-15 14:10:27 +0800

    0615 + 0.0001 (last tau-tau)^2 & PPO update train.py

--- try.sh
+++ try.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo trial 64 echo 1
-python3 run.py --trial 64 -seed 0 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
-echo trial 64 echo 2
-python3 run.py --trial 64 -seed 1 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
-echo trial 64 echo 3
-python3 run.py --trial 64 -seed 2 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
-echo trial 64 echo 4
-python3 run.py --trial 64 -seed 3 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
-echo trial 64 echo 5
-python3 run.py --trial 64 -seed 4 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
+echo trial 65 echo 1
+python3 run.py --trial 65 -seed 0 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
+echo trial 65 echo 2
+python3 run.py --trial 65 -seed 1 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
+echo trial 65 echo 3
+python3 run.py --trial 65 -seed 2 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
+echo trial 65 echo 4
+python3 run.py --trial 65 -seed 3 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
+echo trial 65 echo 5
+python3 run.py --trial 65 -seed 4 -frames 100000 -lr_c 2e-3 -lr_a 2e-3
