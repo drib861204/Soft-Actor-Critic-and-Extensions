89a1f0c04502c68e99da50a30ac9c67cedefce11
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-20 10:37:11 +0800

    0620 77-81 last torque - torque & TD3 update

--- try.sh
+++ try.sh
@@ -1,56 +1,56 @@
 #!/bin/sh
 
-echo trial 72 seed 0
-python3 run.py --trial 72 -seed 0 -w_tau 0.1
-echo trial 72 seed 1
-python3 run.py --trial 72 -seed 1 -w_tau 0.1
-echo trial 72 seed 2
-python3 run.py --trial 72 -seed 2 -w_tau 0.1
-echo trial 72 seed 3
-python3 run.py --trial 72 -seed 3 -w_tau 0.1
-echo trial 72 seed 4
-python3 run.py --trial 72 -seed 4 -w_tau 0.1
+echo trial 77 seed 0
+python3 run.py --trial 77 -seed 0 -w_tau 0.1
+echo trial 77 seed 1
+python3 run.py --trial 77 -seed 1 -w_tau 0.1
+echo trial 77 seed 2
+python3 run.py --trial 77 -seed 2 -w_tau 0.1
+echo trial 77 seed 3
+python3 run.py --trial 77 -seed 3 -w_tau 0.1
+echo trial 77 seed 4
+python3 run.py --trial 77 -seed 4 -w_tau 0.1
 
-echo trial 73 seed 0
-python3 run.py --trial 73 -seed 0 -w_tau 0.01
-echo trial 73 seed 1
-python3 run.py --trial 73 -seed 1 -w_tau 0.01
-echo trial 73 seed 2
-python3 run.py --trial 73 -seed 2 -w_tau 0.01
-echo trial 73 seed 3
-python3 run.py --trial 73 -seed 3 -w_tau 0.01
-echo trial 73 seed 4
-python3 run.py --trial 73 -seed 4 -w_tau 0.01
+echo trial 78 seed 0
+python3 run.py --trial 78 -seed 0 -w_tau 0.01
+echo trial 78 seed 1
+python3 run.py --trial 78 -seed 1 -w_tau 0.01
+echo trial 78 seed 2
+python3 run.py --trial 78 -seed 2 -w_tau 0.01
+echo trial 78 seed 3
+python3 run.py --trial 78 -seed 3 -w_tau 0.01
+echo trial 78 seed 4
+python3 run.py --trial 78 -seed 4 -w_tau 0.01
 
-echo trial 74 seed 0
-python3 run.py --trial 74 -seed 0 -w_tau 0.001
-echo trial 74 seed 1
-python3 run.py --trial 74 -seed 1 -w_tau 0.001
-echo trial 74 seed 2
-python3 run.py --trial 74 -seed 2 -w_tau 0.001
-echo trial 74 seed 3
-python3 run.py --trial 74 -seed 3 -w_tau 0.001
-echo trial 74 seed 4
-python3 run.py --trial 74 -seed 4 -w_tau 0.001
+echo trial 79 seed 0
+python3 run.py --trial 79 -seed 0 -w_tau 0.001
+echo trial 79 seed 1
+python3 run.py --trial 79 -seed 1 -w_tau 0.001
+echo trial 79 seed 2
+python3 run.py --trial 79 -seed 2 -w_tau 0.001
+echo trial 79 seed 3
+python3 run.py --trial 79 -seed 3 -w_tau 0.001
+echo trial 79 seed 4
+python3 run.py --trial 79 -seed 4 -w_tau 0.001
 
-echo trial 75 seed 0
-python3 run.py --trial 75 -seed 0 -w_tau 0.0001
-echo trial 75 seed 1
-python3 run.py --trial 75 -seed 1 -w_tau 0.0001
-echo trial 75 seed 2
-python3 run.py --trial 75 -seed 2 -w_tau 0.0001
-echo trial 75 seed 3
-python3 run.py --trial 75 -seed 3 -w_tau 0.0001
-echo trial 75 seed 4
-python3 run.py --trial 75 -seed 4 -w_tau 0.0001
+echo trial 80 seed 0
+python3 run.py --trial 80 -seed 0 -w_tau 0.0001
+echo trial 80 seed 1
+python3 run.py --trial 80 -seed 1 -w_tau 0.0001
+echo trial 80 seed 2
+python3 run.py --trial 80 -seed 2 -w_tau 0.0001
+echo trial 80 seed 3
+python3 run.py --trial 80 -seed 3 -w_tau 0.0001
+echo trial 80 seed 4
+python3 run.py --trial 80 -seed 4 -w_tau 0.0001
 
-echo trial 76 seed 0
-python3 run.py --trial 76 -seed 0 -w_tau 0.00001
-echo trial 76 seed 1
-python3 run.py --trial 76 -seed 1 -w_tau 0.00001
-echo trial 76 seed 2
-python3 run.py --trial 76 -seed 2 -w_tau 0.00001
-echo trial 76 seed 3
-python3 run.py --trial 76 -seed 3 -w_tau 0.00001
-echo trial 76 seed 4
-python3 run.py --trial 76 -seed 4 -w_tau 0.00001
+echo trial 81 seed 0
+python3 run.py --trial 81 -seed 0 -w_tau 0.00001
+echo trial 81 seed 1
+python3 run.py --trial 81 -seed 1 -w_tau 0.00001
+echo trial 81 seed 2
+python3 run.py --trial 81 -seed 2 -w_tau 0.00001
+echo trial 81 seed 3
+python3 run.py --trial 81 -seed 3 -w_tau 0.00001
+echo trial 81 seed 4
+python3 run.py --trial 81 -seed 4 -w_tau 0.00001
